894cfa2b0ebc7181852d93d87390ea891df43e4b
Zdeněk Brož <zdenek.broz@xtremeware.cz>
2024-10-26 14:16:58 +0200

    update

--- packages/easy-email-extensions/src/AttributePanel/utils/getImg.ts
+++ packages/easy-email-extensions/src/AttributePanel/utils/getImg.ts
@@ -6,7 +6,7 @@ const defaultImagesMap = {
   AttributePanel_01:
     'https://easy-email-m-ryan.vercel.app/images/e22f78f2-aa76-408d-ba94-c95c7abe1908-image.png',
   AttributePanel_02:
-    'https://easy-email-m-ryan.vercel.app/images/3e952a6e-2506-470e-b395-3e0d995157c5.png',
+    'https://www.dancemeetup.com/images/general/attribute_panel_02.png',
   AttributePanel_03:
     'https://easy-email-m-ryan.vercel.app/images/Fi_vI4vyLhTM-Tp6ivq4dR_ieGHk.png',
 };
